7cd54e880eba137bd101ba93c6ee3826ace2a5d4
Konstantin Zhukov <zhukov.konst.sl@gmail.com>
2021-09-04 12:00:51 +0300

    add Wrapper refactoring with hooks

--- src/components/Wrapper/Wrapper.jsx
+++ src/components/Wrapper/Wrapper.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import ContactForm from '../ContactForm/ContactForm';
 import ContactList from '../ContactList/ContactList';
@@ -7,77 +7,68 @@ import Notification from '../Notification/Notification';
 import Section from '../Section/Section';
 import s from './Wrapper.module.css';
 
-class Wrapper extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+function Wrapper() {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
-    parsedContacts && this.setState({ contacts: parsedContacts });
-  }
+  useEffect(() => {
+    const localContacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(localContacts);
+    parsedContacts && setContacts(parsedContacts);
+  }, []);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-  formSubmitHanler = data => {
+  const formSubmitHanler = data => {
     const contact = {
       id: uuidv4(),
       name: data.name,
       number: data.number,
     };
 
-    this.state.contacts.filter(item => item.name === data.name).length > 0
+    contacts.filter(item => item.name === data.name).length > 0
       ? alert(`${contact.name} is already in contacts`)
-      : this.setState(prevState => ({
-          contacts: [...prevState.contacts, contact],
-        }));
+      : setContacts(prevContacts => [...prevContacts, contact]);
   };
 
-  changeFilter = e => {
-    this.setState({ filter: e.currentTarget.value });
+  const changeFilter = e => {
+    setFilter(e.currentTarget.value);
   };
 
-  deleteContactHandler = contactID => {
+  const deleteContactHandler = contactID => {
     console.log(contactID);
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(item => item.id !== contactID),
-    }));
+    setContacts(prevContacts =>
+      prevContacts.filter(item => item.id !== contactID),
+    );
   };
 
-  render() {
-    const { contacts, filter } = this.state;
-    const normalizedFilter = filter.toLowerCase();
-    const filteredContacts = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter),
-    );
+  const normalizedFilter = filter.toLowerCase();
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(normalizedFilter),
+  );
 
-    return (
-      <div className={s.wrapper}>
-        <Section title="Phonebook">
-          <ContactForm onSubmit={this.formSubmitHanler} />
-        </Section>
-        <Section title="Contacts">
-          {contacts.length < 1 ? (
-            <Notification text="Contact list is empty" />
-          ) : (
-            <>
-              <Filter value={filter} onChange={this.changeFilter} />
-              <ContactList
-                contacts={filteredContacts}
-                onDeleteBtnClick={this.deleteContactHandler}
-              />
-            </>
-          )}
-        </Section>
-      </div>
-    );
-  }
+  return (
+    <div className={s.wrapper}>
+      <Section title="Phonebook">
+        <ContactForm onSubmit={formSubmitHanler} />
+      </Section>
+      <Section title="Contacts">
+        {contacts.length < 1 ? (
+          <Notification text="Contact list is empty" />
+        ) : (
+          <>
+            <Filter value={filter} onChange={changeFilter} />
+            <ContactList
+              contacts={filteredContacts}
+              onDeleteBtnClick={deleteContactHandler}
+            />
+          </>
+        )}
+      </Section>
+    </div>
+  );
 }
 
 export default Wrapper;
