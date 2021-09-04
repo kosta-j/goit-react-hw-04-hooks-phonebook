a0e38778c1ad2301649b787ff35e2d29a1b4d05c
Konstantin Zhukov <zhukov.konst.sl@gmail.com>
2021-09-04 12:39:52 +0300

    add ContactForm refactoring with hooks

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,62 +1,68 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import s from './ContactForm.module.css';
 
-class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
+function ContactForm({ onSubmit }) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  handleChange = e => {
+  const handleChange = e => {
     const { name, value } = e.currentTarget;
-    this.setState({ [name]: value });
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        console.log('this data is not supported');
+    }
   };
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    this.props.onSubmit(this.state);
-    this.reset();
+    onSubmit({ name, number });
+    reset();
   };
 
-  reset = () => {
-    this.setState({ name: '', number: '' });
+  const reset = () => {
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          <p className={s.inputTitle}>Name</p>
-          <input
-            type="text"
-            name="name"
-            className={s.input}
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-            required
-            value={this.state.name}
-            onChange={this.handleChange}
-          />
-        </label>
-        <label>
-          <p className={s.inputTitle}>Number</p>
-          <input
-            type="tel"
-            name="number"
-            className={s.input}
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
-            required
-            value={this.state.number}
-            onChange={this.handleChange}
-          />
-        </label>
-        <button type="submit" className={s.submitBtn}>
-          Add contact
-        </button>
-      </form>
-    );
-  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        <p className={s.inputTitle}>Name</p>
+        <input
+          type="text"
+          name="name"
+          className={s.input}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+          required
+          value={name}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        <p className={s.inputTitle}>Number</p>
+        <input
+          type="tel"
+          name="number"
+          className={s.input}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+          required
+          value={number}
+          onChange={handleChange}
+        />
+      </label>
+      <button type="submit" className={s.submitBtn}>
+        Add contact
+      </button>
+    </form>
+  );
 }
 
 export default ContactForm;
--- src/components/ContactList/ContactItem.jsx
+++ src/components/ContactList/ContactItem.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import s from './ContactList.module.css';
 
 function ContactItem({ name, number, children }) {
@@ -8,5 +9,11 @@ function ContactItem({ name, number, children }) {
     </li>
   );
 }
+
+ContactItem.propTypes = {
+  name: PropTypes.string,
+  number: PropTypes.string,
+  children: PropTypes.node,
+};
 
 export default ContactItem;
